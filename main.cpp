123a3a24b38390148932deb3bf0296f1f4fdf42b
harangozo.gabor <theharipresents@gmail.com>
2024-09-29 16:46:47 +0200

    Hibak kijavitva

--- main.cpp
+++ main.cpp
@@ -4,25 +4,30 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];  // Helytelen: N_ELEMENTS név helyett NELEMENTS lett használva, ami nem létezik.
-    std::cout << '1-100 ertekek duplazasa';
-    for (int i = 0;)  // Helytelen: A for ciklusból hiányzik a feltétel és az inkrementálás.
-    {
-        b[i] = i * 2;
-    }
-    for (int i = 0; i; i++)  // Helytelen: A ciklus feltétele mindig hamis lesz, mert i kezdetben 0, így a ciklus nem fut le.
-    {
-        std::cout << "Ertek:";
-    }
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
 
-    for (int i = 0; i < N_ELEMENTS, i++)  // Helytelen: Vessző (,) helyett pontosvesszőt (;) kellene használni a for feltételében.
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+        b[i] = (i + 1) * 2;
+    }
+
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+        std::cout << "Ertek: " << b[i] << std::endl;
+    }
+
+    std::cout << "Atlag szamitasa: " << std::endl;
+
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         atlag += b[i];
     }
 
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0; //memory leak, nincs torolve a b ponter
+
+    delete[] b;
+    return 0;
 }
